820ca7c2c1615683ae2a97d038a7646f82575968
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2021-03-28 11:00:09 +0300

    added option to update record and more specified error logging

--- src/import.js
+++ src/import.js
@@ -40,6 +40,8 @@ export default function () {
 
   return async message => {
     const record = new MarcRecord(JSON.parse(message.content.toString()), {subfieldValues: false});
+    const [f001] = record.get('001');
+    const UPDATE = f001.value !== undefined;
     const title = getRecordTitle(record);
     const standardIdentifiers = getRecordStandardIdentifiers(record);
 
@@ -50,14 +52,31 @@ export default function () {
 
     try {
       logger.log('info', 'Importing record to Melinda...');
-      const {recordId: id} = await apiClient.create(record, {unique: true});
+      if (UPDATE) {
+        const {recordId: id} = await apiClient.create(record, {unique: true});
+        logger.log('info', `Created new record ${id}`);
+        return {status: RECORD_IMPORT_STATE.CREATED, metadata: {id, title, standardIdentifiers}};
+      }
 
-      logger.log('info', `Created new record ${id}`);
-      return {status: RECORD_IMPORT_STATE.CREATED, metadata: {id, title, standardIdentifiers}};
+      const {payload: id} = await apiClient.update(record, f001.value);
+      logger.log('info', `Updated record ${id}`);
+      return {status: RECORD_IMPORT_STATE.UPDATED, metadata: {id, title, standardIdentifiers}};
+
     } catch (err) {
       if (err.status) {
         if (err.status === httpStatus.CONFLICT) {
+          logger.log('error', 'Got expected conflict response');
           return {status: RECORD_IMPORT_STATE.DUPLICATE, metadata: {matches: err.payload, title, standardIdentifiers}};
+        }
+
+        if (error.status === httpStatus.UNPROCESSABLE_ENTITY) {
+          logger.log('error', 'Got expected unprosessable entity response');
+          return {status: RECORD_IMPORT_STATE.INVALID, metadata: {validationMessages: error.payload, title, standardIdentifiers}};
+        }
+
+        if (error.status === httpStatus.FORBIDDEN) {
+          logger.log('error', 'Got expected unprosessable entity response');
+          return {status: RECORD_IMPORT_STATE.INVALID, metadata: {validationMessages: error.payload, title, standardIdentifiers}};
         }
 
         throw new Error(`Melinda REST API error: ${err.status} ${err.payload || ''}`);
